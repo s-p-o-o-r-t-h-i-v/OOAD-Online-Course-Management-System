b0e1681ef34058fcfc700e9038daf7ab3f8fbd97
TechieTweet <imswathim@gmail.com>
2026-04-12 16:34:44 +0530

    Implemented Chat history feature

---
+++ src/main/java/ooad/repository/ChatMessageRepository.java
@@ -0,0 +1,9 @@
+package ooad.repository;
+
+import ooad.model.ChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+    List<ChatMessage> findByStudentName(String studentName);
+}
